4191b23877dc21410957a20bbc4d55e1681d431e
Pawel <pwwgil@gmail.com>
2019-12-15 09:52:17 +0100

    [init] - project skeleton

---
+++ src/main/java/pl/pawel/springgit2/Springgit2Application.java
@@ -0,0 +1,13 @@
+package pl.pawel.springgit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springgit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springgit2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pawel/springgit2/Springgit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawel.springgit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springgit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
