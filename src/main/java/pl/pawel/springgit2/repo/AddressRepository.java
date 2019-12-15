aa640f1f737631495b474c605e4f1bb266baf12f
Pawel <pwwgil@gmail.com>
2019-12-15 12:32:21 +0100

    [add] - AddressRepository new method

--- src/main/java/pl/pawel/springgit2/repo/AddressRepository.java
+++ src/main/java/pl/pawel/springgit2/repo/AddressRepository.java
@@ -3,5 +3,8 @@ package pl.pawel.springgit2.repo;
 import org.springframework.data.repository.CrudRepository;
 import pl.pawel.springgit2.domain.Address;
 
+import java.util.List;
+
 public interface AddressRepository extends CrudRepository<Address, Long> {
+    List<Address> findAllOrOrderByCity();
 }
